b6fd2a515d8ca19e8e968965f001d7310c3d5144
fanghonghui <a6551142@163.com>
2019-02-17 23:06:46 +0800

    springcloud eureka zuul config demo upms cms 模块

---
+++ microservice-demo/src/main/java/com/fanghh/springcloud/MicroserviceDemoApplication.java
@@ -0,0 +1,15 @@
+package com.fanghh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class MicroserviceDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceDemoApplication.class, args);
+    }
+
+}
---
+++ microservice-upms/src/main/java/com/fanghh/springcloud/MicroserviceUpmsApplication.java
@@ -0,0 +1,13 @@
+package com.fanghh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceUpmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroserviceUpmsApplication.class, args);
+    }
+
+}
